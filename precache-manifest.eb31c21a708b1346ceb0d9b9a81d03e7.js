05c1bb903389d3629ddc453aaa0374d2dff9a889
Ranvir Prasad <ranvir.prasad@gmail.com>
2022-10-24 21:59:45 +0530

    Updates

---
+++ precache-manifest.eb31c21a708b1346ceb0d9b9a81d03e7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "5aefd03ed91b706ae232",
+    "url": "/myweb/static/css/main.ca467354.chunk.css"
+  },
+  {
+    "revision": "5aefd03ed91b706ae232",
+    "url": "/myweb/static/js/main.daeb3b1c.chunk.js"
+  },
+  {
+    "revision": "6e1255d8d355e2ba56c1",
+    "url": "/myweb/static/js/runtime~main.df14b1fb.js"
+  },
+  {
+    "revision": "f7426fde0e537c2280b6",
+    "url": "/myweb/static/css/2.6ad0499f.chunk.css"
+  },
+  {
+    "revision": "f7426fde0e537c2280b6",
+    "url": "/myweb/static/js/2.06545c5d.chunk.js"
+  },
+  {
+    "revision": "92cf7c32f4a48b614f30",
+    "url": "/myweb/static/js/3.a9a7ad6a.chunk.js"
+  },
+  {
+    "revision": "be6228998e31904c0d42d148597680fa",
+    "url": "/myweb/static/media/logo.be622899.svg"
+  },
+  {
+    "revision": "c8a84da570e1de51a1ce776d6dda55be",
+    "url": "/myweb/index.html"
+  }
+];
